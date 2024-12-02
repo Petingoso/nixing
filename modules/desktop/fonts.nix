63ac1550cd097208ceb961b6e7573df911f94447
Petingoso <petingavasco@protonmail.com>
2024-12-02 22:08:08 +0000

    added new nerdfonts syntax

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -2,21 +2,15 @@
   lib,
   pkgs,
   ...
-}: let
-  inherit (lib.attrsets) attrValues;
-in {
-  fonts.packages = attrValues {
-    inherit
-      (pkgs)
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-emoji
-      corefonts
-      vistafonts
-      ;
-    nerdfonts = pkgs.nerdfonts.override {
-      fonts = ["JetBrainsMono" "Iosevka"];
-    };
-  };
+}: {
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+    corefonts
+    vistafonts
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.iosevka
+  ];
 }

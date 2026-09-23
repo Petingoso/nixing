999dd3e751cd0c22c2e5fcfc1a4970390d5a1c00
Petingoso <petingavasco@protonmail.com>
2026-09-23 10:21:09 +0100

    chore HM

--- options/other/home-manager.nix
+++ options/other/home-manager.nix
@@ -21,7 +21,7 @@ in
     home-manager = {
       useUserPackages = true;
       useGlobalPkgs = true;
-      backupFileExtension = "hm_backup";
+      backupFileExtension = "bkup-home-manager-${toString inputs.self.lastModifiedDate }";
       users.${username} = {
         programs = {
           home-manager.enable = true;
